20d038ab0c98f455fee45db8fe42c2b024034f14
Jean-Romain Garnier <github@jean-romain.com>
2020-06-05 12:17:35 +0200

    Allow setting more options from CLI
    Added options for blacklist, data_folder and statistics

--- fact_extractor/helperFunctions/program_setup.py
+++ fact_extractor/helperFunctions/program_setup.py
@@ -11,12 +11,15 @@ from version import __VERSION__
 def setup_argparser(name, description, command_line_options, version=__VERSION__):
     parser = argparse.ArgumentParser(description='{} - {}'.format(name, description))
     parser.add_argument('-V', '--version', action='version', version='{} {}'.format(name, version))
-    parser.add_argument('-l', '--log_file', help='path to log file', default=None)
-    parser.add_argument('-L', '--log_level', help='define the log level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default=None)
-    parser.add_argument('-d', '--debug', action='store_true', default=False, help='print debug messages')
+    parser.add_argument('-o', '--output', dest='data_folder', help='Path to output directory', default=None)
+    parser.add_argument('-l', '--log_file', help='Path to log file', default=None)
+    parser.add_argument('-L', '--log_level', help='Define the log level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default=None)
+    parser.add_argument('-d', '--debug', action='store_true', help='Print debug messages', default=False)
     parser.add_argument('-C', '--config_file', help='set path to config File', default='{}/main.cfg'.format(get_config_dir()))
+    parser.add_argument('--exclude', metavar='GLOB_PATTERN', action='append', help='Exclude files paths that match %(metavar)s.', default=[])
+    parser.add_argument('--blacklist', metavar='MIME_TYPE', action='append', help='Exclude files with %(metavar)s.', default=[])
+    parser.add_argument('--disable-statistics', action='store_false', dest='statistics', help='Don\'t compute statistics or check for unpack data loss', default=None)
     parser.add_argument('FILE_PATH', type=str, help='Path to file that should be extracted')
-    parser.add_argument('--exclude', metavar='GLOB_PATTERN', action='append', default=[], help='Exclude files paths that match %(metavar)s.')
     return parser.parse_args(command_line_options[1:])
 
 
@@ -45,7 +48,18 @@ def load_config(config_file):
     return config
 
 def merge_options(arguments, config):
-    # Merge exclude list from config and CLI arguments
+    # Merge exclude lists from config and CLI arguments
     exclude = read_list_from_config(config, 'unpack', 'exclude')
     config['unpack']['exclude'] = ", ".join(exclude + arguments.exclude)
+
+    blacklist = read_list_from_config(config, 'unpack', 'blacklist')
+    config['unpack']['blacklist'] = ", ".join(blacklist + arguments.blacklist)
+
+    # Allow overriding options from CLI
+    if arguments.data_folder:
+        config['unpack']['data_folder'] = arguments.data_folder
+
+    if arguments.statistics is not None:
+        config['ExpertSettings']['statistics'] = str(arguments.statistics)
+
     return config
