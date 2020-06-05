7a8bd3c84af8766b130a23e87258a7b8e18d92c7
Jean-Romain Garnier <github@jean-romain.com>
2020-06-05 10:41:35 +0200

    Ignore __pycache__ folders in plugin search

--- fact_extractor/helperFunctions/plugin.py
+++ fact_extractor/helperFunctions/plugin.py
@@ -18,6 +18,10 @@ def _get_plugin_src_dirs(base_dir):
     plugin_dirs = get_dirs_in_dir(str(plug_in_base_path))
     plugins = []
     for plugin_path in plugin_dirs:
+        # Ignore cache directories
+        if Path(plugin_path).parts[-1] == "__pycache__":
+            continue
+        # Check the plugin has a valid structure
         plugin_code_dir = Path(plugin_path, 'code')
         if plugin_code_dir.is_dir():
             plugins.append(str(plugin_code_dir))
